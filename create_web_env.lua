43e1665e5ef7daa4c33698df0aff2e1ed3d6fec7
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-04-14 02:33:18 +0200

    Update create_web_env.lua

--- create_web_env.lua
+++ create_web_env.lua
@@ -9,7 +9,7 @@ style_css_c = ""
 start_bat_c = ""
 http_server_oy_c = ""
 localhost_8000_c = "<meta http-equiv="refresh" content="0.1;url=http://localhost:8000">"
-other_vs_code_md_c = ""
+other_vs_code_md_c = "# Other VSCode\n\nIf you have installed VSCode in another directory and\nwant to use this installation, change the name of this\nfile from `other_vs_code.md` to `other_vs_code` and\nopen the `run_server_for_testing.bat` file and check\nline 10.\n\n```\nstart "" "C:\D4R4\.can_delete\Microsoft VS Code\Code.exe" "%cd%"\n```\n\n`C:\D4R4\.can_delete\Microsoft VS Code\Code.exe` is the\nused folder path, replace it with your folder path.\n\nfinished!\n"
 create_web_env_info_md_c = ""
 
 -- functions
